efa54c4f96fd7780b89abc2c0f22bbacd04b7c84
Yeseo0502 <5252yeseo@gmail.com>
2025-09-21 23:52:00 +0900

    예제연습

---
+++ Test1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>프로그램의 기본 형태</title>
+</head>
+<body>
+    3 + 4 =
+    <?php
+        echo 3+4,"<br>";
+    ?>
+
+    3 + 4 = <?= 3+4 //이렇게도 php활용 가능함.?>
+</body>
+</html>
